93a51aae453da47324e89acddd9a06f45c0b4e24
DanAtLetsGame <dan@letsgame.org>
2024-07-07 00:38:48 -0700

    ready now?

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -18,7 +18,7 @@ const config = {
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: '/',
   organizationName: 'DanAtLetsGame',
-  projectName: 'DanAtLetsGame',
+  projectName: 'DanAtLetsGame.github.io',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
